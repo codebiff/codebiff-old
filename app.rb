14d7634f67afd50ebe358736f967f9972bb5a761
Dave Nicholson <fatnic388@gmail.com>
2012-01-11 19:47:02 +0000

    seperate Array methods ito own file

--- app.rb
+++ app.rb
@@ -1,17 +1,5 @@
 include JADOF
 
-class Array
-  
-  def paginate(page=1, per_page=10)
-    each_slice(per_page).to_a[page.to_i-1]
-  end
-
-  def pages(per_page=10)
-    (count / per_page.to_f).ceil
-  end
-
-end
-
 # Constants
 BLOG_TITLE       = "CodeBiff"
 BLOG_DESCRIPTION = "Broadcasting from the arse end of the web"
--- config.ru
+++ config.ru
@@ -1,6 +1,7 @@
 require "sinatra"
 require "jadof"
 require "yaml"
+require "./lib/array"
 require "./app"
 
 run Sinatra::Application
