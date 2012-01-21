7a186f8f43716f47ca075d33591b1621ba623df7
Code Biff <me@codebiff.com>
2012-01-21 11:23:32 +0000

    pubdate and comments

--- app.rb
+++ app.rb
@@ -62,6 +62,7 @@ helpers do
   include Rack::Utils
   alias_method :h, :escape_html
 
+  # Get the current page from the query string. If none then is page 1
   def get_page
     case page = params[:page].to_i
       when 0; 1
@@ -70,14 +71,17 @@ helpers do
     end
   end
   
+  # format time (1st January, 2012)
   def tidy_date(date)
     date.strftime("#{date.day.ordinalize} of %B, %Y")
   end
 
+  # format time for html time tag
   def geek_date(date)
     date.strftime("%Y-%m-%dT%H:%M:%S%z") 
   end
 
+  # Pluralize any word (2, post) (5, dice, die)
   def pluralize(n, singular, plural=nil)
     if n == 1 
       "1 #{singular}"
@@ -88,6 +92,7 @@ helpers do
     end
   end
 
+  # Loads partial view into template. Required vriables into locals
   def partial(template, locals = {})
     erb(template, :layout => false, :locals => locals)
   end
