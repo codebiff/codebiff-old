1cc9919be420e3d0ad36e5b63bfa357f8741a3ca
Code Biff <me@codebiff.com>
2012-02-05 19:47:37 +0000

    preloading image

--- public/js/main.js
+++ public/js/main.js
@@ -27,6 +27,9 @@ $(function(){
    });
 
    // change search arrow when input entered
+   img1 = new Image(); // preload image to stop flash
+   img1.src = "../images/arrow-curve.png";
+
    $("#side-search").keyup(function(){
       if ($(this).val().trim() == "") {
         $(".sidebar-search button").css("background-image", "url(../images/arrow-curve-gray.png)");
