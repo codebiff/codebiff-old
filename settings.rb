2a35606f0372bedf41992f31996ffe3c0748a9f1
Dave Nicholson <dave@codebiff.com>
2012-04-26 19:53:17 +0100

    added cache commands

--- settings.rb
+++ settings.rb
@@ -4,3 +4,8 @@ set :blog_description, 	"Broadcasting from the arse end of the web"
 set :disqus_dev, 				ENV['DEVELOPMENT'] ? 1 : 0
 
 set :per_page, 					10
+
+set :root,              File.expand_path(File.dirname(__FILE__))
+set :public,            File.expand_path(File.dirname(__FILE__) + "/public") 
+set :cache_output_dir,  File.expand_path(File.dirname(__FILE__) + "/public/cache") 
+set :cache_enabled,     true
