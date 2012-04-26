b510cb7330f1ebd74d6629fca40c67f5aeff157d
Dave Nicholson <dave@codebiff.com>
2012-04-26 21:19:44 +0100

    fucksake

--- settings.rb
+++ settings.rb
@@ -1,5 +1,5 @@
-set :root,              File.expand_path(File.dirname(__File__))
-set :public,            File.expand_path(File.dirname(__File__) + "/public")
+set :root,              File.expand_path(File.dirname(__FILE__))
+set :public,            File.expand_path(File.dirname(__FILE__) + "/public")
 
 set :blog_title, 				"CodeBiff"
 set :blog_description, 	"Broadcasting from the arse end of the web"
