8a0387d95ede511a400b7d52e7168083e74b9dbc
Dave Nicholson <dave@codebiff.com>
2012-04-27 22:33:58 +0100

    trying to fix crappy caching

--- routes.rb
+++ routes.rb
@@ -11,7 +11,7 @@ Post.formatters['redcarpet'] = lambda do |text|
     end
   end
 
-  options = {:autolink => true, :no_intra_emphasis => true, :fenced_code_blocks => true, :strikethrough => true, :superscript => true}
+  options = {:autolink => true, :lax_html_block => true, :no_intra_emphasis => true, :fenced_code_blocks => true, :strikethrough => true, :superscript => true}
   markdown = Redcarpet::Markdown.new(AlbinoHTML, options)
   markdown.render(text)
 end
