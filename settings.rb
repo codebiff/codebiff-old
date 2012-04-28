b1d00fd0bc1546ab4b12854272d7edc223fb0a5a
Dave Nicholson <dave@codebiff.com>
2012-04-28 06:44:09 +0100

    removed horrible disqus line

--- settings.rb
+++ settings.rb
@@ -4,7 +4,7 @@ set :public,            File.expand_path(File.dirname(__FILE__) + "/public")
 set :blog_title, 				"CodeBiff"
 set :blog_description, 	"Broadcasting from the arse end of the web"
 
-set :disqus_dev, 				ENV['DEVELOPMENT'] ? 1 : 0
+set :disqus_dev, 				0
 
 set :per_page, 					50
 
